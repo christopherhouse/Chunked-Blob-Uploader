a3d937a708dac57e03134371a9af4e25658025f5
Chris House <chhouse@microsoft.com>
2022-10-13 13:08:06 -0500

    aded resp model

---
+++ ChunkedBlobUploader/Models/Responses/UploadChunkResponse.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace ChunkedBlobUploader.Models.Responses
+{
+    public class UploadChunkResponse
+    {
+        public UploadChunkResponse()
+        {
+            UploadedBlockIds = new List<string>();
+        }
+
+        public List<string> UploadedBlockIds { get; set; }
+    }
+}
