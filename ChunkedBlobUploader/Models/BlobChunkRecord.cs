31caf13a946de1876f3f7a777fcc0ee4b5c25594
Chris House <chhouse@microsoft.com>
2022-10-13 13:11:03 -0500

    added table entity

---
+++ ChunkedBlobUploader/Models/BlobChunkRecord.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Azure;
+using Azure.Data.Tables;
+
+namespace ChunkedBlobUploader.Models
+{
+    public class BlobChunkRecord : ITableEntity
+    {
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+        public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; }
+    }
+}
