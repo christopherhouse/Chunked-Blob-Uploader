4d33c57a6f542900dfc6a88d7dfa674d35e30f00
Chris House <chhouse@microsoft.com>
2022-10-13 13:32:39 -0500

    initial add of function

--- ChunkedBlobUploader/UploadBlobChunk.cs
+++ ChunkedBlobUploader/UploadBlobChunk.cs
@@ -1,6 +1,16 @@
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
+using System.Text;
 using System.Threading.Tasks;
+using Azure.Data.Tables;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs.Specialized;
+using ChunkedBlobUploader.Models;
+using ChunkedBlobUploader.Models.Requests;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
@@ -9,9 +19,10 @@ using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Microsoft.WindowsAzure.Storage.Table;
 using Newtonsoft.Json;
 
-namespace ChunkedBlobUploader
+namespace ChunkedBlobUploader.Functions
 {
     public class UploadBlobChunk
     {
@@ -22,27 +33,84 @@ namespace ChunkedBlobUploader
             _logger = log;
         }
 
-        [FunctionName("UploadBlobChunk")]
+        [FunctionName(nameof(UploadBlobChunk))]
         [OpenApiOperation(operationId: "Run", tags: new[] { "name" })]
         [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
         [OpenApiParameter(name: "name", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **Name** parameter")]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
         public async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req)
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = "uploadblobchunk/{fileName}")] HttpRequest req,
+            string fileName,
+            [Table("%chunktable%", Connection = "blobStorageConnectionString")] TableClient chunks,
+            [Table("%chunktable%")] IAsyncCollector<BlobChunkRecord> chunkToAdd)
         {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            UploadChunkRequest uploadRequest = null;
+            using (var streamReader = new StreamReader(req.Body))
+            {
+                var jsonString = await streamReader.ReadToEndAsync();
+                uploadRequest = JsonConvert.DeserializeObject<UploadChunkRequest>(jsonString);
+            }
+            var newBlockId = await UploadChunk(fileName, uploadRequest.ContentAsByteArray(), uploadRequest.IsFinalChunk);
+            var uploadedChunks = await GetBlobChunksByPartitionKey(fileName, chunks);
 
-            string name = req.Query["name"];
+            var blockIdsToReturn = new List<string>();
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            if (uploadedChunks.Any())
+            {
+                blockIdsToReturn.AddRange(blockIdsToReturn);
+            }
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            blockIdsToReturn.Add(newBlockId);
 
-            return new OkObjectResult(responseMessage);
+            if (uploadRequest.IsFinalChunk)
+            {
+                await CommitBlobChunks(fileName, blockIdsToReturn, "application/zip");
+            }
+
+            return new OkObjectResult(blockIdsToReturn);
+        }
+
+        private static async Task<IEnumerable<BlobChunkRecord>> GetBlobChunksByPartitionKey(string partitionKey,
+            TableClient chunkTable)
+        {
+            var blocks = new List<BlobChunkRecord>();
+            var queryResults = chunkTable.QueryAsync<BlobChunkRecord>($"PartitionKey eq '{partitionKey}'");
+            await foreach (var chunk in queryResults)
+            {
+                blocks.Add(chunk);
+            }
+
+            return blocks;
+        }
+
+        private static async Task CommitBlobChunks(string fileName, IList<string> blockIds, string mimeType)
+        {
+            var options = new CommitBlockListOptions
+            {
+                HttpHeaders = new BlobHttpHeaders
+                {
+                    ContentType = mimeType
+                }
+            };
+            var blobServiceClient = new BlobServiceClient(Environment.GetEnvironmentVariable(""));
+            var containerClient = blobServiceClient.GetBlobContainerClient(Environment.GetEnvironmentVariable(""));
+            var blockClient = containerClient.GetBlockBlobClient(fileName);
+            await blockClient.CommitBlockListAsync(blockIds, options);
+        }
+
+        private static async Task<string> UploadChunk(string fileName, byte[] blobChunkContentBytes, bool isFinalChunk)
+        {
+            var blockId = Convert.ToBase64String(Encoding.UTF8.GetBytes(Guid.NewGuid().ToString()));
+            var blobServiceClient =
+                new BlobServiceClient(Environment.GetEnvironmentVariable("blobStorageConnectionString"));
+            var containerName = Environment.GetEnvironmentVariable("blobContainer");
+            var blobContainerClient = blobServiceClient.GetBlobContainerClient(containerName);
+            var blockClient = blobContainerClient.GetBlockBlobClient(fileName);
+
+            using var ms = new MemoryStream(blobChunkContentBytes);
+            await blockClient.StageBlockAsync(blockId, ms);
+
+            return blockId;
         }
     }
 }
